622f4d5a7d2cb3629b04cfb6e1e9c42024adb589
Keanu Clark <da1andonlyone13@gmail.com>
2023-01-23 13:50:20 -0500

    Update push_updates.sh

--- push_updates.sh
+++ push_updates.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-DOC_ID=""
-TEST_MSG="sent_from_bash_script"
-python append_to_file.py $TEST_MSG $DOC_ID
+echo "Checking comment section for new entries"
+
+#include todoist task id, can be found with url
+TASK=0000000
+
+#API key can be found in integrations in settings
+curl https://api.todoist.com/sync/v9/items/get \
+    -H "Authorization: Bearer 000000000000000"\
+    -d item_id=$TASK \
+    -o \
+    > ./tmp/task_dump.txt
+
+python handle_task_dump.py
+
+#Doc ID can be found in doc url /d/000000/edit
+DOC_ID="0000000000000"
+python append_to_file.py -f $DOC_ID
+echo
+
